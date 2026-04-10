4277eb3fd88eef260814dafd8be599a4cc78e1f9
Felix Lubbe <fe.lubbe@gmail.com>
2026-04-11 01:49:41 +0200

    Fetch stb.

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -12,18 +12,6 @@
 mkdir -p deps/3rd-party
 cd deps/3rd-party
 
-# Download cpu_features 0.10.1 and put it in deps/3rd-party/cpu_features
-wget https://github.com/google/cpu_features/archive/refs/tags/v0.10.1.tar.gz
-tar -xf v0.10.1.tar.gz
-rm v0.10.1.tar.gz
-mv cpu_features-0.10.1 cpu_features
-
-# Download libmorton
-wget https://github.com/Forceflow/libmorton/archive/refs/tags/v0.2.12.zip
-unzip v0.2.12.zip
-rm v0.2.12.zip
-mv libmorton-0.2.12 libmorton
-
 # Download stb
 git clone https://github.com/nothings/stb.git --depth=1
 
@@ -36,21 +24,6 @@ wget https://github.com/simdjson/simdjson/releases/download/v4.2.4/singleheader.
 echo "b4fd52b7e60e881050893613367516c0ebdf4aa50f18abe1a50819adfe750d9a singleheader.zip" | sha256sum -c -
 unzip singleheader.zip -d simdjson/singleheader
 rm singleheader.zip
-
-#
-# --- 1st party ---
-#
-cd ..
-
-# concurrency utility library
-git clone https://github.com/flubbe/concurrency_utils.git
-
-# mathematics library
-git clone https://github.com/flubbe/ml.git
-cd ml
-chmod +x ./scripts/*
-./scripts/pre-build.sh
-cd ..
 
 #
 # change directory to project root.
