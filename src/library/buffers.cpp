79e9433d830174a2ec31dbd50145d3ab7733f21d
Felix Lubbe <fe.lubbe@gmail.com>
2026-09-12 17:52:02 +0200

    Fix buffer index validity check.

--- src/library/buffers.cpp
+++ src/library/buffers.cpp
@@ -45,7 +45,7 @@ void UpdateIndexBuffer(std::uint32_t id, const std::vector<std::uint32_t>& data)
 void UpdateAttributeBuffer(std::uint32_t id, const std::vector<ml::vec4>& data)
 {
     ASSERT_INTERNAL_CONTEXT;
-    if(id >= impl::global_context->vertex_attribute_buffers.size())
+    if(!impl::global_context->vertex_attribute_buffers.contains(id))
     {
         impl::global_context->last_error = error::invalid_value;
         return;
@@ -78,7 +78,7 @@ void DeleteAttributeBuffer(std::uint32_t id)
 {
     ASSERT_INTERNAL_CONTEXT;
 
-    if(id < impl::global_context->vertex_attribute_buffers.size())
+    if(impl::global_context->vertex_attribute_buffers.contains(id))
     {
         impl::global_context->vertex_attribute_buffers[id].data.clear(); /* FIXME the .data member access here prevents more unification with the delete_buffer function above? */
         impl::global_context->vertex_attribute_buffers.erase(id);
@@ -95,7 +95,7 @@ void EnableAttributeBuffer(std::uint32_t id, std::uint32_t slot)
     impl::render_context* context = impl::global_context;
 
     // check if id and slot are valid.
-    if(id < context->vertex_attribute_buffers.size() && slot < context->active_vabs.max_size())
+    if(context->vertex_attribute_buffers.contains(id) && slot < context->active_vabs.max_size())
     {
         // check if we need to allocate a new slot.
         if(slot >= context->active_vabs.size())
@@ -118,7 +118,7 @@ void DisableAttributeBuffer(std::uint32_t id)
     impl::render_context* context = impl::global_context;
 
     // check that BufferId is valid.
-    if(id < context->vertex_attribute_buffers.size())
+    if(context->vertex_attribute_buffers.contains(id))
     {
         auto& buf = context->vertex_attribute_buffers[id];
         if(buf.slot >= 0 && static_cast<std::size_t>(buf.slot) < context->active_vabs.size())
